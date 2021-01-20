98a86ee66b35e8c99efeed44721b0eca0319d2bb
Loren Gordon <lorengordon@users.noreply.github.com>
2021-01-19 16:27:45 -0800

    Removes create_keys variable

--- variables.tf
+++ variables.tf
@@ -1,11 +1,4 @@
-variable "create_keys" {
-  description = "Controls whether to create a set of keys"
-  type        = bool
-  default     = true
-}
-
 variable "keys" {
   description = "Schema list of KMS Keys, consisting of alias, (OPTIONAL) description, (OPTIONAL) deletion_window_in_days, (OPTIONAL) policy, (OPTIONAL) enable_key_rotation. The enable_key_rotation variable is a boolean."
   type        = list(any)
-  default     = []
 }
